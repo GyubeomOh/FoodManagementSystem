dedd05eb80e6ed044bcf7318d760aafa82800a60
GyubeomOh <ogb0707@gmail.com>
2021-03-29 09:37:19 +0900

    add comment

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -6,7 +6,7 @@ public class MenuManager {
 		int num = 0;
 		Scanner input = new Scanner(System.in);
 
-		while (num !=6) {
+		while (num !=6) { // 6번이 아니면 실행하도록 유도
 			System.out.println("1. Add food");
 			System.out.println("2. Delete food");
 			System.out.println("3. Edit food");
@@ -17,50 +17,50 @@ public class MenuManager {
 			num = input.nextInt();
 			
 			if (num == 1) {
-				addFood();
+				addFood(); // 1을 입력하면 public static void addFood() 에서 내용을 가져옴
 			}
 			if (num == 2) {
-				deleteFood();
+				deleteFood(); // 2을 입력하면 public static void deleteFood() 에서 내용을 가져옴
 			}
 			if (num == 3) {
-				editFood();
+				editFood(); // 3을 입력하면 public static void editFood() 에서 내용을 가져옴
 			}
 			if (num == 4) {
-				viewFood();
+				viewFood(); // 4을 입력하면 public static void viewFood() 에서 내용을 가져옴
 			}
-			else {
+			else { // 6번은 while에서 이미 배제했기에 else에는 5번인 경우만 포함되는데 5번인 경우엔 continue를 실행하게함
 				continue;
 			}
 		}
 	} 
 	
-	public static void addFood() {
+	public static void addFood() { // addFood 함수 정의
 		Scanner input = new Scanner(System.in);
 		System.out.print("Student ID:");
-		int studentID = input.nextInt();
+		int studentID = input.nextInt(); // studentID 를 입력받음
 		System.out.print("Student name:");
-		String studentName = input.next();
-		System.out.print("Email address:");
-		String studentEmail = input.next();
+		String studentName = input.next(); // studentName 을 입력받음
+		System.out.print("Email address:"); 
+		String studentEmail = input.next(); // studentEmail 을 입력받음
 		System.out.println(studentEmail);
-		System.out.print("Phone number:");
-		String studentPhone = input.next();
+		System.out.print("Phone number:"); 
+		String studentPhone = input.next(); // studentPhone 을 입력받음
 		
 	}
 	
-	public static void deleteFood() {
+	public static void deleteFood() { // deleteFood 함수 정의
 		Scanner input = new Scanner(System.in);
 		System.out.print("Student ID:");
 		int studentID = input.nextInt();
 	}
 	
-	public static void editFood() {
+	public static void editFood() { // editFood 함수 정의
 		Scanner input = new Scanner(System.in);
 		System.out.print("Student ID:");
 		int studentID = input.nextInt();
 	}
 	
-	public static void viewFood() {
+	public static void viewFood() { // viewFood 함수 정의
 		Scanner input = new Scanner(System.in);
 		System.out.print("Student ID:");
 		int studentID = input.nextInt();
