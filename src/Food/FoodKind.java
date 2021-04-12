f75918c1f28baf6e10fa3601bb050739906c49ed
GyubeomOh <ogb0707@gmail.com>
2021-04-12 19:54:35 +0900

    add details and coding such as inheritance code, setters and getters

---
+++ src/Food/FoodKind.java
@@ -0,0 +1,8 @@
+package Food;
+
+public enum FoodKind { // enum 은 들어가는 종류가 0,1,2,3 이랑 숫자로 정해지는데요 그 부분에 대해서 문자로 표현할 수 있게함.
+	Korean,
+	Chinese,
+	Japanese,
+	FastFood
+}
---
+++ src/Food/KoreanFood.java
@@ -0,0 +1,38 @@
+package Food;
+
+import java.util.Scanner;
+
+public class KoreanFood extends Food {
+	public void getFoodInput(Scanner input) {
+		System.out.print("Food name : ");
+		String name = input.next();
+		this.setName(name);
+		
+		System.out.print("restaurant name : ");
+		String restaurant = input.next();
+		this.setRestaurant(restaurant);
+		
+		char answer = 'x';
+		while (answer != 'y' && answer != 'Y' && answer != 'n' && answer != 'N') {
+			System.out.print("Phone number exist? (Y or N)");
+			answer = input.next().charAt(0);
+			if (answer == 'y' || answer =='Y') {
+				System.out.print("Phone number : ");
+				String phone = input.next();
+				this.setPhone(phone);
+				break;
+			}
+			else if (answer == 'n' || answer =='N') {
+				this.setPhone("");
+				break;
+			}
+			else {
+				System.out.print("Phone number exist? (Y or N)");
+			}
+		}
+		
+		System.out.print("Food price : "); 
+		int price = input.nextInt();
+		this.setPrice(price);
+	}
+}
