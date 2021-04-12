9bafd5b56e87bade7581776b5e5219f229553d40
GyubeomOh <ogb0707@gmail.com>
2021-04-12 19:50:37 +0900

    detailed the code

---
+++ src/Food/Food.java
@@ -0,0 +1,83 @@
+package Food;
+
+import java.util.Scanner;
+
+public class Food {
+	protected FoodKind kind = FoodKind.Korean;	
+	protected String name;
+	protected String restaurant;
+	protected String phone;
+	protected int price;
+	
+	public Food() {
+	}
+
+	public Food(String name, String restaurant, String phone, int price) {
+		this.name = name;
+		this.restaurant = restaurant;
+		this.phone = phone;
+		this.price = price;
+	}
+
+	public FoodKind getKind() {
+		return kind;
+	}
+
+	public void setKind(FoodKind kind) {
+		this.kind = kind;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRestaurant() {
+		return restaurant;
+	}
+
+	public void setRestaurant(String restaurant) {
+		this.restaurant = restaurant;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public void printInfo() {
+		System.out.println("name:" + name + " restaurant:" + restaurant + " phone:" + phone + " price:" + price);
+	}
+	
+	public void getFoodInput(Scanner input) {
+		System.out.print("Food name : ");
+		String name = input.next();
+		this.setName(name);
+		
+		System.out.print("restaurant name : ");
+		String restaurant = input.next();
+		this.setRestaurant(restaurant);
+		
+		System.out.print("Phone number : ");
+		String phone = input.next();
+		this.setPhone(phone);
+		
+		System.out.print("Food price : "); 
+		int price = input.nextInt();
+		this.setPrice(price);
+	}
+}
--- src/FoodManager.java
+++ src/FoodManager.java
@@ -1,78 +1,136 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import Food.Food;
+import Food.KoreanFood;
+import Food.ChineseFood;
+import Food.JapaneseFood;
+import Food.FastFood;
+
 public class FoodManager {
-	Food food;	
+	ArrayList<Food> foods = new ArrayList<Food>();	
 	Scanner input;
 	FoodManager(Scanner input){
 		this.input= input;
 	}
 	
 	public void addFood() { // addFood 함수 정의
-		food = new Food();
-		for (int i = 0; i<food.name.length; i++) {
-			System.out.print("Food name : ");
-			food.name[i] = input.next();
-			System.out.print("restaurant name : ");
-			food.restaurant[i] = input.next();
-			System.out.print("Phone number : "); 
-			food.phone[i] = input.nextInt();
-			System.out.print("Food price : "); 
-			food.price[i] = input.nextInt();
+		int kind = 0;
+		Food food;
+		while (kind != 1 && kind != 2) {
+			System.out.println("1 for Korean Food");
+			System.out.println("2 for Chinese Food");
+			System.out.println("3 for Japanese Food");
+			System.out.println("4 for Fast Food");
+			System.out.print("Select num for Food Kind between 1 and 4 : ");
+			kind = input.nextInt();
+			if (kind == 1) {
+				food = new KoreanFood();
+				food.getFoodInput(input);
+				foods.add(food);
+				break;
+			}
+			
+			else if (kind == 2) {
+				food = new ChineseFood();
+				food.getFoodInput(input);
+				foods.add(food);
+				break;
+			}
+			
+			else if (kind == 3) {
+				food = new JapaneseFood();
+				food.getFoodInput(input);
+				foods.add(food);
+				break;
+			}
+			
+			else if (kind == 4) {
+				food = new FastFood();
+				food.getFoodInput(input);
+				foods.add(food);
+				break;
+			}
+			
+			else {
+				System.out.print("Select num for Food Kind : ");
+			}
+			
 		}
+
 	}
 	
 	public void deleteFood() { // deleteFood 함수 정의
 		System.out.print("Food name : ");
 		String foodname = input.next();
-		for(int i = 0; i<food.name.length; i++) {
-			if (food.name[i].equals(foodname)) {
-				food.name[i] = null;
-				food.restaurant[i] = null;
-				food.phone[i] = (Integer) null;
-				food.price[i] = (Integer) null;
-				System.out.println("the food is deleted");
+		int index = -1;
+		for(int i=0 ; i<foods.size(); i++) {
+			if (foods.get(i).getName().equals(foodname)) {
+				index = i;
+				break;
 			}
-			else {
-				System.out.println("the food has not been registered");
-				return;
-			}
+		}
+		if (index >= 0) {
+			foods.remove(index);
+			System.out.println("the food " + foodname + " is deleted");
+		}
+		else {
+			System.out.println("the food has not been registered");
+			return;
 		}
 	}
 	
 	public void editFood() { // editFood 함수 정의
 		System.out.print("Food name : ");
 		String foodname = input.next();
-		for (int i = 0; i<food.name.length; i++) {
-			if (food.name[i].equals(foodname)) {
+		for(int i=0 ; i<foods.size(); i++) {
+			Food food = foods.get(i);
+			if (food.getName().equals(foodname)) {
 				int num = 0;
-				while (num !=4) {
-					System.out.println("1. edit restaurant");
-					System.out.println("2. edit phone");
-					System.out.println("3. edit price");
-					System.out.println("4. EXIT");
+				while (num !=5) {
+					System.out.println("1. edit name");
+					System.out.println("2. edit restaurant");
+					System.out.println("3. edit phone");
+					System.out.println("4. edit price");
+					System.out.println("5. EXIT");
+					System.out.println("Select one number between 1 - 5");
 					num = input.nextInt();
 					if (num == 1) {
-						System.out.print("restaurant name : ");
-						food.restaurant[i] = input.next();
-					}
+						System.out.print("Food name : ");
+						String name = input.next();
+						food.setName(name);
+					}					
 					if (num == 2) {
-						System.out.print("Phone number : "); 
-						food.phone[i] = input.nextInt();
+						System.out.print("Restaurant name : ");
+						String restaurant = input.next();
+						food.setRestaurant(restaurant);
 					}
 					if (num == 3) {
+						System.out.print("Phone number : "); 
+						String phone = input.next();
+						food.setPhone(phone);
+					}
+					if (num == 4) {
 						System.out.print("Food price : "); 
-						food.price[i] = input.nextInt();
+						int price = input.nextInt();
+						food.setPrice(price);
 					}
 					else {
 						continue;
 					}
 				}
+				break;
 			}
 		}
 	}
 	
 	public void viewFood() { // viewFood 함수 정의
-		food.printInfo();
+//		System.out.print("Food name :");
+//		String Food.name = input.next();
+		System.out.println("# of registered foods: " + foods.size());
+		for(int i=0 ; i<foods.size(); i++) {
+			foods.get(i).printInfo();
+		}
 	}
 }
 
