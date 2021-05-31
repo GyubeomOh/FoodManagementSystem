4268cfd5369f8761b0e1617e31b71f214fc0518c
GyubeomOh <ogb0707@gmail.com>
2021-05-31 20:17:16 +0900

    added GUI package and related classes

---
+++ src/gui/FoodAdder.java
@@ -0,0 +1,51 @@
+package gui;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+public class FoodAdder extends JFrame {
+	
+	public FoodAdder() {
+		JPanel panel = new JPanel(new SpringLayout());
+		panel.setLayout(new SpringLayout());
+		
+		JLabel labelName = new JLabel("Name : ", JLabel.TRAILING);
+		JTextField fieldName = new JTextField(10);
+		labelName.setLabelFor(fieldName);
+		panel.add(labelName);
+		panel.add(fieldName);
+		
+		JLabel labelRestaurant = new JLabel("Restaurant : ", JLabel.TRAILING);
+		JTextField fieldRestaurant = new JTextField(10);
+		labelRestaurant.setLabelFor(fieldRestaurant);
+		panel.add(labelRestaurant);
+		panel.add(fieldRestaurant);
+		
+		JLabel labelPhone = new JLabel("Phone : ", JLabel.TRAILING);
+		JTextField fieldPhone = new JTextField(10);
+		labelPhone.setLabelFor(fieldPhone);
+		panel.add(labelPhone);
+		panel.add(fieldPhone);
+		
+		JLabel labelPrice = new JLabel("Price : ", JLabel.TRAILING);
+		JTextField fieldPrice = new JTextField(10);
+		labelPrice.setLabelFor(fieldPrice);
+		panel.add(labelPrice);
+		panel.add(fieldPrice);
+		
+		panel.add(new JButton("save"));
+		panel.add(new JButton("cancel"));
+		
+		SpringUtilities.makeCompactGrid(panel, 5, 2, 6, 6, 6, 6);
+		
+		this.setSize(300, 300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.setContentPane(panel);
+		this.setVisible(true);
+	}
+}
---
+++ src/gui/GUITest.java
@@ -0,0 +1,11 @@
+package gui;
+
+public class GUITest {
+
+	public static void main(String[] args) {
+		MenuSelection menuselection = new MenuSelection();
+		FoodAdder foodadder = new FoodAdder();
+		FoodViewer foodviewer = new FoodViewer();
+	}
+
+}
