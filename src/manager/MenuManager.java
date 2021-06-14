c2bf4ad86852b41f4b9731a2b789c293ca82127f
GyubeomOh <ogb0707@gmail.com>
2021-06-14 16:52:38 +0900

    edited words

--- src/manager/MenuManager.java
+++ src/manager/MenuManager.java
@@ -64,10 +64,10 @@ public class MenuManager {
 	}
 	
 	public static void showMenu() {
-		System.out.println("1. Add food!");
-		System.out.println("2. Delete food!");
-		System.out.println("3. Edit food!");
-		System.out.println("4. View food!");
+		System.out.println("1. Add food");
+		System.out.println("2. Delete food");
+		System.out.println("3. Edit food");
+		System.out.println("4. View food");
 		System.out.println("5. Exit");
 		System.out.println("Select one number between 1 - 5:");
 	} 
